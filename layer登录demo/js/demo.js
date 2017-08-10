3fc49bc703497fc15b63000adf5f4c5acbe0c67b
ronghaoZHI <895576961@qq.com>
2017-08-10 19:38:41 +0800

    8.09

---
+++ layer登录demo/js/demo.js
@@ -0,0 +1,58 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+
+function showLoginbox() {
+    $("#txtTel").val("");
+    $("#txtPwd").val("");
+    $("#txtCode").val("");
+    layer.open({
+        type: 1,//表示弹出的是一个div
+        title: "登录",
+        area: ["393px", "320px"],//设置弹窗的高度和宽度，注意：不能在css里面设置
+        content: $("#loginbox")
+    });
+}
+
+function login() {
+    var tel = $.trim($("#txtTel").val());//获取到手机号
+    var pwd = $.trim($("#txtPwd").val());//获取到密码
+    var code = $.trim($("#txtCode").val());//获取到验证码
+    var rtel = /^(13|14|15|16|17|18)\d{9}$/;//验证手机号的正则表达式
+    var rpwd = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/; //验证密码，由数字和字母组成长度为6~12
+    var rcode = /^[0-9a-zA-Z]{4}$/; //验证验证码
+    if (!rtel.test(tel))
+    {
+        layer.msg("手机号格式不正确", {
+            time: 1000,
+            icon:5
+        });
+        return;
+    }
+    if (!rpwd.test(pwd)) {
+        layer.msg("密码格式不正确", {
+            time: 1000,
+            icon: 5
+        });
+        return;
+    }
+    if (!rcode.test(code)) {
+        layer.msg("验证码格式不正确", {
+            time: 1000,
+            icon: 5
+        });
+        return;
+    }
+    if (tel == "13800000000" && pwd == "admin123") {
+        layer.msg("登录成功", {
+            time: 1000,
+            icon: 6
+        }, function () {
+            location.href = "http://www.ruanmou.net";
+        });
+    }
+    else {
+        layer.msg("手机号或者密码错误", {
+            time: 1000,
+            icon: 5
+        });
+    }
+}
---
+++ 砸蛋demo/js/demo.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="jquery-1.10.2.min.js" />
+
+$(function () {
+    var iNum = Math.floor(Math.random() * 4 + 1);//获取到1~4之间的随机整数
+    var icount = 0;
+    $(".egg").mouseover(function () {//鼠标移到蛋上之后
+        var left = $(this).offset().left + 110;//获取到当前鼠标放上去的蛋的left+蛋和锤子之间的间距=锤子距离浏览器的left
+        $("#t").css("left", left);
+    });
+
+    $(".egg").click(function () {
+        if (icount == 4) {//蛋砸完了
+            document.getElementById("a3").play();
+            return;
+        }
+        if ($(this).attr("data-isbreak") == "false") {
+            icount++;
+            $(this).css("background-image", "url(img/egg_2.png)");
+
+            var i = $(this).index() + 1;//获取到当前点击的蛋是第几个蛋
+            if (i == iNum) {//中奖
+                document.getElementById("a2").play();
+            }
+            else {//没有中奖
+                document.getElementById("a1").play();
+            }
+            $(this).attr("data-isbreak", "true");
+        }
+    });
+
+
+});
